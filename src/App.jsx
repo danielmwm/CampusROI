f270a06cb6c92083fabfe47919dbf73b12d0df50
Daniel Winter-Minott <danielwinter-minott@Daniels-MacBook-Air.local>
2026-06-06 21:56:10 -0400

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -23,6 +24,7 @@ export default function App() {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </BrowserRouter>
   );
 }
